f6cd508dd6bbb4c25817e0a0fd82f44cc96d6823
takshittarun <98863252+takshittarun@users.noreply.github.com>
2022-02-09 21:56:50 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World! :::Polling test2");
+    System.out.println("Hello World! :::Triggered by webhook");
     }
 }
